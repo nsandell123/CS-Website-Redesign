be8158fa8467465367a04c62cd2b3bd4345195d5
Chris D <cheesecakecgcontact@gmail.com>
2019-07-11 12:18:06 -0400

    Big Commit #2

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 $(document).ready(function()
 {
-  switchPage("home-page");
+  if (localStorage.getItem("lastPage") != null)
+    switchPage(localStorage.getItem("lastPage"));
+  else
+    switchPage("home-page")
+
+  if (localStorage.getItem("lastData") != null)
+    getData(localStorage.getItem("lastData"));
   //bind - automatically calls the function (event)
 
 });
@@ -19,26 +25,35 @@ $(document).ready(function()
 
 
 function switchPage(id) {
+  if (id == "home-page") {
+    $("#back-button").css('visibility','hidden');
+    $("footer").css('visibility','hidden');
+  } else {
+    $("#back-button").css('visibility','visible');
+    $("footer").css('visibility','visible');
+  }
   var pages = $(".page").toArray();
   for (p of pages) {
     console.log(p.id);
     if (p.id == id) {
       $("#" + p.id).show(150);
+      localStorage.setItem("lastPage", id)
     } else {
       $("#" + p.id).hide(250);
     }
   }
+  
 }
 
 // Function 1: will be used to retrieve data from document
   function getData(link) {
+    localStorage.setItem("lastData", link);
     $.get(link, function(responseText) {
     displayData(responseText);
 });
 
 
   }
-
 
 
 // Function 2: will be used to load data that was retrieved onto Website
